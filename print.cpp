d2aaad79c7db64b4a798636213018f1d0be75b63
Tymur Sotnikov <timursotnikov21@gmail.com>
2024-04-22 01:35:23 +0300

    Fixed logging namespace

--- print.cpp
+++ print.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <sstream>
 #include "print.h"
+#include <array>
 
 namespace logging {
     void Log::printAuthorInfo() {
@@ -9,17 +10,16 @@ namespace logging {
     }
 
     void Log::printTaskInfo() {
-        std::cout << "Task: check if quadrilateral fits in the triangle" << std::endl << "------------------------------" << std::endl;
+        std::cout << "Task: check if quadrilateral fits in the triangle" << std::endl
+                  << "------------------------------" << std::endl;
     }
 
-    void printEnd() {
+    void Log::printEnd() {
         std::cout << "Task completed." << std::endl;
     }
 
-    Log::~Log() = default;
-
-    std::tuple<double, double> inputTriangle1() {
-        double tr_point1_x, tr_point1_y;
+    std::array<std::tuple<double, double>, 3> inputTriangle() {
+        double tr_point1_x, tr_point1_y, tr_point2_x, tr_point2_y, tr_point3_x, tr_point3_y;
 
         std::cout << "Enter first point of triangle (x, y) separated by space:" << std::endl;
         std::string input_line;
@@ -30,41 +30,29 @@ namespace logging {
 
         std::tuple<double, double> tr_point1 = std::make_tuple(tr_point1_x, tr_point1_y);
 
-        return tr_point1;
-    }
-
-    std::tuple<double, double> inputTriangle2() {
-        double tr_point2_x, tr_point2_y;
-
         std::cout << "Enter second point of triangle (x, y) separated by space:" << std::endl;
-        std::string input_line;
+        std::string input_line2;
         std::getline(std::cin, input_line);
 
-        std::istringstream iss(input_line);
-        iss >> tr_point2_x >> tr_point2_y;
+        std::istringstream iss2(input_line);
+        iss2 >> tr_point2_x >> tr_point2_y;
 
         std::tuple<double, double> tr_point2 = std::make_tuple(tr_point2_x, tr_point2_y);
 
-        return tr_point2;
-    }
-
-    std::tuple<double, double> inputTriangle3() {
-        double tr_point3_x, tr_point3_y;
-
         std::cout << "Enter third point of triangle (x, y) separated by space:" << std::endl;
-        std::string input_line;
+        std::string input_line3;
         std::getline(std::cin, input_line);
 
-        std::istringstream iss(input_line);
-        iss >> tr_point3_x >> tr_point3_y;
+        std::istringstream iss3(input_line);
+        iss3 >> tr_point3_x >> tr_point3_y;
 
         std::tuple<double, double> tr_point3 = std::make_tuple(tr_point3_x, tr_point3_y);
 
-        return tr_point3;
+        return {tr_point1, tr_point2, tr_point3};
     }
 
-    std::tuple<double, double> inputQuadrilateral1() {
-        double q_point1_x, q_point1_y;
+    std::array<std::tuple<double, double>, 4> inputQuadrilateral() {
+        double q_point1_x, q_point1_y, q_point2_x, q_point2_y, q_point3_x, q_point3_y, q_point4_x, q_point4_y;
 
         std::cout << "Enter first point of quadrilateral (x, y) separated by space:" << std::endl;
         std::string input_line;
@@ -75,50 +63,33 @@ namespace logging {
 
         std::tuple<double, double> q_point1 = std::make_tuple(q_point1_x, q_point1_y);
 
-        return q_point1;
-    }
-
-    std::tuple<double, double> inputQuadrilateral2() {
-        double q_point2_x, q_point2_y;
-
         std::cout << "Enter second point of quadrilateral (x, y) separated by space:" << std::endl;
-        std::string input_line;
+        std::string input_line2;
         std::getline(std::cin, input_line);
 
-        std::istringstream iss(input_line);
-        iss >> q_point2_x >> q_point2_y;
+        std::istringstream iss2(input_line);
+        iss2 >> q_point2_x >> q_point2_y;
 
         std::tuple<double, double> q_point2 = std::make_tuple(q_point2_x, q_point2_y);
 
-        return q_point2;
-    }
-
-    std::tuple<double, double> inputQuadrilateral3() {
-        double q_point3_x, q_point3_y;
-
         std::cout << "Enter third point of quadrilateral (x, y) separated by space:" << std::endl;
-        std::string input_line;
+        std::string input_line3;
         std::getline(std::cin, input_line);
 
-        std::istringstream iss(input_line);
-        iss >> q_point3_x >> q_point3_y;
+        std::istringstream iss3(input_line);
+        iss3 >> q_point3_x >> q_point3_y;
 
         std::tuple<double, double> q_point3 = std::make_tuple(q_point3_x, q_point3_y);
 
-        return q_point3;
-    }
-
-    std::tuple<double, double> inputQuadrilateral4() {
-        double q_point4_x, q_point4_y;
-
         std::cout << "Enter fourth point of quadrilateral (x, y) separated by space:" << std::endl;
-        std::string input_line;
+        std::string input_line4;
         std::getline(std::cin, input_line);
 
-        std::istringstream iss(input_line);
-        iss >> q_point4_x >> q_point4_y;
+        std::istringstream iss4(input_line);
+        iss4 >> q_point4_x >> q_point4_y;
 
         std::tuple<double, double> q_point4 = std::make_tuple(q_point4_x, q_point4_y);
 
-        return q_point4;}
+        return {q_point1, q_point2, q_point3, q_point4};
+    }
 }
--- print.h
+++ print.h
@@ -1,23 +1,16 @@
 #pragma once
-
 #ifndef PRINT_H
 #define PRINT_H
+#include <array>
 
 namespace logging { // Простір імен для виводу інформації
-    class Log {
-    public:
-        void printAuthorInfo();
-        void printTaskInfo();
-        void printEnd();
-        ~Log();
+    struct Log {
+        static void printAuthorInfo();
+        static void printTaskInfo();
+        static void printEnd();
     };
-    std::tuple<double, double> inputTriangle1();
-    std::tuple<double, double> inputTriangle2();
-    std::tuple<double, double> inputTriangle3();
-    std::tuple<double, double> inputQuadrilateral1();
-    std::tuple<double, double> inputQuadrilateral2();
-    std::tuple<double, double> inputQuadrilateral3();
-    std::tuple<double, double> inputQuadrilateral4();
+    std::array<std::tuple<double, double>, 3> inputTriangle();
+    std::array<std::tuple<double, double>, 4> inputQuadrilateral();
 }
 
 #endif
